15bb4db6fb8e9cc0947af297cfc6503693e90756
Erkin303 <maxmudjonoverkin55@gmail.com>
2021-07-31 09:29:10 +0300

    first commit

--- src/data/index.js
+++ src/data/index.js
@@ -3,7 +3,17 @@ import uy from './uy'
 
 const data = [
     { category: "detskiy-mir", data: detskiyMir },
-    { category: "uy", data: uy }
+    { category: "uy", data: uy },
+    { category: "transport", data: [] },
+    { category: "ish", data: [] },
+    { category: "jonivorlar", data: [] },
+    { category: "uy-va-bog", data: [] },
+    { category: "elektronika", data: [] },
+    { category: "biznes", data: [] },
+    { category: "moda", data: [] },
+    { category: "hobbi", data: [] },
+    { category: "tekin", data: [] },
+    { category: "ayirboshlash", data: [] }
 ]
 
 export default data;
